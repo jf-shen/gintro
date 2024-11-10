5bbdd04c8bf7b596c059a8c4a4084b9842590daa
jf-shen <sweetfishhcl@sina.com>
2024-11-10 18:55:15 +0800

    add qt

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gintro',
-    version='0.1.2',
+    version='0.1.3',
     author='jfshen',
     packages=find_packages(),
     install_requires=[
