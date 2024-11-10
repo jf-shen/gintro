39e6ab3721247dbb1f3bc6969899ca52d3f9ac86
jf-shen <sweetfishhcl@sina.com>
2024-11-10 23:17:38 +0800

    fix bug

--- gintro/mail.py
+++ gintro/mail.py
@@ -1,7 +1,7 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
-from decorators import timeit
+from .decorators import timeit
 
 
 @timeit
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gintro',
-    version='0.1.4',
+    version='0.1.5',
     author='jfshen',
     packages=find_packages(),
     install_requires=[
