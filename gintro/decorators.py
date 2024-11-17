c4fb283c5a18eb86bf940fb7d5c35b8c56e6fc5a
jf-shen <sweetfishhcl@sina.com>
2024-11-17 11:15:39 +0800

    add smarttime

--- gintro/decorators.py
+++ gintro/decorators.py
@@ -2,7 +2,13 @@ import time
 
 
 def smart_time(sec):
-    pass
+    if sec < 120:
+        return '%.2f sec' % sec
+    min = sec / 60
+    if min < 120:
+        return '%.2f min' % min
+    hour = min / 60
+    return '%.2f h' % hour
 
 
 def timeit(fn):
@@ -10,8 +16,8 @@ def timeit(fn):
         start_time = time.time()
         print("[timeit] start running function [%s]" % fn.__name__)
         output = fn(*args, **kwargs)
-        print("[timeit] finish running function [%s], time elapsed = %.3f secs" %
-              (fn.__name__, time.time() - start_time))
+        print("[timeit] finish running function [%s], time elapsed = %s" %
+              (fn.__name__, smart_time(time.time() - start_time)))
         return output
     return wrapper
 
