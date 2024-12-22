a5ffbf8e9476ad88f93be62797e2eb80c28e863d
jf-shen <sweetfishhcl@sina.com>
2024-12-22 22:40:44 +0800

    add my_env

--- build/lib/gintro/__init__.py
+++ build/lib/gintro/__init__.py
@@ -2,5 +2,4 @@ from .stream import Stream
 from .decorators import timeit, time_it
 from .mail import send_mail
 
-
-from basic import logit, sigmoid, get_auc
+from .basic import logit, sigmoid, get_auc
--- gintro/my_env.py
+++ gintro/my_env.py
@@ -1,3 +1,8 @@
+"""
+Command:
+from gintro.my_env import *
+"""
+
 import matplotlib.pyplot as plt
 import seaborn as sns
 from IPython import display
@@ -8,5 +13,10 @@ display.set_matplotlib_formats('svg')
 sns.set_theme()
 plt.figure(figsize=[8, 5])
 
-plt.rcParams["font.sans-serif"]=["SimHei"]  #设置字体
-plt.rcParams["axes.unicode_minus"]=False    #该语句解决图像中的“-”负号的乱码问题
+
+"""
+Possible Error: 
+findfont: Generic family 'sans-serif' not found because none of the following families were found: SimHei
+"""
+# plt.rcParams["font.sans-serif"]=["SimHei"]  # 设置字体
+# plt.rcParams["axes.unicode_minus"]=False    # 该语句解决图像中的“-”负号的乱码问题
